640342bcd7315ebace32e2a49adc4191c59b615a
leemj0112 <163072105+leemj0112@users.noreply.github.com>
2024-05-22 15:43:32 +0900

    SelectScene 완료

---
+++ Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+[CreateAssetMenu]
+public class CharacterInfo: ScriptableObject
+{
+    public string Name;
+    public string Feature;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SelectChange.cs
+++ Assets/Scripts/SelectChange.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SelectChange : MonoBehaviour
@@ -6,14 +7,48 @@ public class SelectChange : MonoBehaviour
 
     [Header("Infer")]
     public Text NameTxt;
-    public Text FeatureTxttxt;
+    public Text FeatureTxt;
     public Image Charimage;
 
     [Header("Charater")]
     public GameObject[] Charcters; //전사, 궁수, 마법사
+    public CharacterInfo[] characterInfos;
     private int charindex;
 
-    public void seectCharaterRtn(string btnNase)
+    [Header("GameStart")]
+    public GameObject GameStart;
+    public Text GameCountTxt;
+    private bool isPlayButtonClicked = false;
+    private float gameCount = 5f;
+
+    private static string CharactorName;
+
+    private void Start()
+    {
+        SetPanelInfo();
+    }
+
+    private void Update()
+    {
+        if (isPlayButtonClicked)
+        {
+            gameCount -= Time.deltaTime;
+            if (gameCount <= 0)
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+            GameCountTxt.text = $"곧 게임이 시작됩니다... \n {gameCount:F1}";
+        }
+    }
+
+    public void PlayBtn()
+    {
+        GameStart.SetActive(true);
+        isPlayButtonClicked = true;
+        CharactorName = Charcters[charindex].name;
+    }
+
+    public void selectCharaterRtn(string btnNase)
     {
 
         Charcters[charindex].SetActive(false);
@@ -31,7 +66,15 @@ public class SelectChange : MonoBehaviour
             charindex = charindex < 0 ? charindex + Charcters.Length : charindex;
         }
 
-        Debug.Log( $"캐릭터 인덱스: {charindex}");
+        Debug.Log($"캐릭터 인덱스: {charindex}");
         Charcters[charindex].SetActive(true);
+        SetPanelInfo();
+    }
+
+    private void SetPanelInfo()
+    {
+        NameTxt.text = characterInfos[charindex].Name;
+        FeatureTxt.text = characterInfos[charindex].Feature;
+        Charimage.sprite = Charcters[charindex].GetComponent<SpriteRenderer>().sprite;
     }
 }
